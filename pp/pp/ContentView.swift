a812501305e9b848bd8d7e622ba9ef94fcdf35ce
Jihyun <75922558+jkim68888@users.noreply.github.com>
2024-04-03 17:54:29 +0900

    [feat] 탭뷰 구현

--- pp/pp/ContentView.swift
+++ pp/pp/ContentView.swift
@@ -9,78 +9,45 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
+    @State private var isLoggedIn: Bool = false
+    
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+        TabView {
+            DiaryView()
+                .tabItem {
+                    VStack {
+                        Image(systemName: "folder.badge.person.crop")
+                        Text("나의 일기")
                     }
                 }
-                .onDelete(perform: deleteItems)
+            if isLoggedIn {
+                CommunityView()
+                    .tabItem {
+                        VStack {
+                            Image(systemName: "person.3.fill")
+                            Text("커뮤니티")
+                        }
+                    }
+            } else {
+                LoginView()
+                    .tabItem {
+                        VStack {
+                            Image(systemName: "person.3.fill")
+                            Text("커뮤니티")
+                        }
+                    }
             }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
+            SettingView()
+                .tabItem {
+                    VStack {
+                        Image(systemName: "gear")
+                        Text("설정")
                     }
                 }
-            }
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
         }
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
 #Preview {
-    ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    ContentView()
 }
