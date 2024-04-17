e3f2e3083dd5547bd038dc9bd70d13167c0110d4
LimJaeHyeon9298 <limjh9298@gmail.com>
2024-04-17 09:40:12 +0900

    Initial Commit

--- pp/pp/View/Community/CommunityView.swift
+++ pp/pp/View/Community/CommunityView.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 struct CommunityView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello, World!")
+      //  print("community")
     }
 }
 
