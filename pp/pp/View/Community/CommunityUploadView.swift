660ed79fa1299f4d7b91d0a8ce5dc8bd4dd4d47f
LimJaeHyeon9298 <limjh9298@gmail.com>
2024-06-17 15:40:59 +0900

    [fix] 게시물 작성하다 뒤로가기 누르고 다시 들어오면 이전 작성하던 데이터 남아있는 에러 해결 및 작성중 뒤로가기 누르면 알림창 뜨는 기능 구현

--- pp/pp/View/Community/CommunityUploadView.swift
+++ pp/pp/View/Community/CommunityUploadView.swift
@@ -14,6 +14,7 @@ struct CommunityUploadView: View {
     @State private var selectedIndex: Int = 0
     @State private var isShownSheet = false
     @State private var showAlert = false
+    @State private var showUnsavedChangesAlert = false
     @Environment(\.dismiss) private var dismiss
     
     let maxPhotosToSelect = 10
@@ -40,7 +41,6 @@ struct CommunityUploadView: View {
                     .frame(width: 120, height: 40)
                     .background(Color.accentColor)
                     .cornerRadius(5)
-               //   .disabled(!isValidInput(vm.title) || !isValidInput(vm.contents) || vm.uiImages.isEmpty)
                 }
                 .padding(.horizontal, 16)
                 .padding(.top, 20)
@@ -59,6 +59,32 @@ struct CommunityUploadView: View {
             }
         }
         .navigationBarTitle("업로드", displayMode: .inline)
+        .navigationBarBackButtonHidden(true) // 기본 네비게이션 뒤로가기 버튼 숨기기
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                Button(action: {
+                    if !vm.title.isEmpty || !vm.contents.isEmpty || !vm.uiImages.isEmpty {
+                        showUnsavedChangesAlert = true
+                    } else {
+                        dismiss()
+                    }
+                }) {
+                    Image(systemName: "chevron.left")
+                    Text("뒤로")
+                }
+            }
+        }
+        .alert(isPresented: $showUnsavedChangesAlert) {
+            Alert(
+                title: Text("경고"),
+                message: Text("현재 작성 중인 내용이 전부 사라집니다. 계속하시겠습니까?"),
+                primaryButton: .destructive(Text("확인")) {
+                    vm.reset()
+                    dismiss()
+                },
+                secondaryButton: .cancel(Text("취소"))
+            )
+        }
         .sheet(isPresented: $isShownSheet) {
             ImageCropper(image: $vm.uiImages[selectedIndex])
         }
@@ -69,7 +95,7 @@ struct CommunityUploadView: View {
     }
 
     private func isValidInput(_ input: String) -> Bool {
-        // 유효성 검사 - 공백문자만 있는지,Html 태그 방지,최소한 하나의 공백 아닌 문자 포함해야함
+        // 유효성 검사 - 공백문자만 있는지, Html 태그 방지, 최소한 하나의 공백 아닌 문자 포함해야 함
         let pattern = "^(?!\\s*$)(?!.*<[^>]+>).+"
         let regex = try? NSRegularExpression(pattern: pattern)
         let range = NSRange(location: 0, length: input.utf16.count)
--- pp/pp/View/Diary/DiaryUploadView.swift
+++ pp/pp/View/Diary/DiaryUploadView.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Financial CB on 2024/04/03.
 //
-
 import SwiftUI
 import PhotosUI
 import Mantis
@@ -15,6 +14,8 @@ struct DiaryUploadView: View {
     @State private var selectedIndex: Int = 0
     @State private var isShownSheet = false
     @State private var showAlert = false
+    @State private var showUnsavedChangesAlert = false
+    @State private var shouldDismiss = false
     @Environment(\.dismiss) private var dismiss
     
     let maxPhotosToSelect = 10
@@ -44,7 +45,6 @@ struct DiaryUploadView: View {
                     .frame(width: 120, height: 40)
                     .background(Color.accentColor)
                     .cornerRadius(5)
-         //   .disabled(!isValidInput(vm.title) || !isValidInput(vm.contents) || vm.uiImages.isEmpty)
                 }
                 .padding(.horizontal, 16)
                 .padding(.top, 20)
@@ -63,6 +63,37 @@ struct DiaryUploadView: View {
             }
         }
         .navigationBarTitle("업로드", displayMode: .inline)
+        .navigationBarBackButtonHidden(true)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                Button(action: {
+                    if !vm.title.isEmpty || !vm.contents.isEmpty || !vm.uiImages.isEmpty {
+                        showUnsavedChangesAlert = true
+                    } else {
+                        dismiss()
+                    }
+                }) {
+                    Image(systemName: "chevron.left")
+                    Text("뒤로")
+                }
+            }
+        }
+        .alert(isPresented: $showUnsavedChangesAlert) {
+            Alert(
+                title: Text("경고"),
+                message: Text("현재 작성 중인 내용이 전부 사라집니다. 계속하시겠습니까?"),
+                primaryButton: .destructive(Text("확인")) {
+                    vm.clearStates()
+                    dismiss()
+                },
+                secondaryButton: .cancel(Text("취소"))
+            )
+        }
+        .onDisappear {
+            if shouldDismiss {
+                vm.clearStates()
+            }
+        }
         .sheet(isPresented: $isShownSheet, content: {
             ImageCropper(image: $vm.uiImages[selectedIndex])
         })
@@ -73,7 +104,7 @@ struct DiaryUploadView: View {
     }
 
     private func isValidInput(_ input: String) -> Bool {
-        // 유효성 검사 - 공백문자만 있는지,Html 태그 방지,최소한 하나의 공백 아닌 문자 포함해야함
+        // 유효성 검사 - 공백문자만 있는지, Html 태그 방지, 최소한 하나의 공백 아닌 문자 포함해야 함
         let pattern = "^(?!\\s*$)(?!.*<[^>]+>).+"
         let regex = try? NSRegularExpression(pattern: pattern)
         let range = NSRange(location: 0, length: input.utf16.count)
